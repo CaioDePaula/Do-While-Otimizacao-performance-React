72981a99e6dfa3aff4bbbe4ee3a3a413643a851c
CaioDePaula <caio.teodoro@ftdata.com.br>
2020-12-14 23:45:18 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useCallback} from 'react';
+import arrayMove from 'array-move'
+import {Feed} from './components/feed';
+import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos/')
+      .then(response => response.json())
+      .then(data => setPosts(data));
+  }, []);
+
+  const handleSort = useCallback(({ oldIndex, newIndex }) => {
+    setPosts(arrayMove(posts, oldIndex, newIndex));
+  }, [posts]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Feed lockAxis="y" posts={posts} onSortEnd={handleSort} />
     </div>
   );
 }
---
+++ src/components/post.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { SortableElement } from 'react-sortable-hoc';
+
+import '../styles/Post.css';
+
+const areEqual = (prevProps, nextProps) => {
+    return prevProps.post.id === nextProps.post.id
+}
+
+export const Post = SortableElement(React.memo(({ post, imgStyle }) => {
+    return (
+        <div className="post">
+            {post.albumId === 2 && <div>Album 2</div>}
+            <img src={post.url} alt={post.title} style={imgStyle} />
+            <h3>{post.title}</h3>
+        </div>
+    );
+}, areEqual))
